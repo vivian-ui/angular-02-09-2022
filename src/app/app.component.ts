6b6c7536eff245933872e9c3e885cd7161b617a6
vivian-ui <66747981+vivian-ui@users.noreply.github.com>
2022-09-02 20:31:17 -0300

    get e set interpolação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   name = 'Vivian da Cunha ';
   nascimento = 1975;
+  counter = 0;
+  timer = setInterval(() => {
+    this.counter = this.counter + 1;
+  });
+
+  getIdade() {
+    return 2022 - this.nascimento;
+  }
 }
