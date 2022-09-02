debd988dcfd93946bbba1221cd701860b348a482
vivian-ui <66747981+vivian-ui@users.noreply.github.com>
2022-09-02 20:25:02 -0300

    interpolação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'VivianApp ';
+  name = 'Vivian da Cunha ';
+  nascimento = 1975;
 }
